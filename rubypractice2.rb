d643404f08833d2f7404293f4b9372cefb9205cc
chikau0730 <39984712+chikau0730@users.noreply.github.com>
2018-06-11 21:10:45 +0900

    Update rubypractice2.rb

--- rubypractice2.rb
+++ rubypractice2.rb
@@ -1,16 +1,13 @@
-ary =[1,2,3]
-
-def toi2(ary)
-  a = Proc.new do |*args|
-    args.map{|e| e+e}
-  end
-  b = a[*ary]
-
-  sum = 0
-  b.each do |k|
-    sum+= k
-  end
-  puts sum
+def give_me_block
+  yield
+   sum = 0
+    @b.each do |k|
+     sum += k
+    end  
+   puts sum
 end
 
-toi2(ary)
+give_me_block do
+  ary = [1,2,3]
+  @b = ary.map{|e| e + e}
+end
