a78428ef9f4eff95b4eea4b3cc670223af6e7a17
chikau0730 <39984712+chikau0730@users.noreply.github.com>
2018-06-11 21:03:12 +0900

    Update rubypractice1.rb

--- rubypractice1.rb
+++ rubypractice1.rb
@@ -1,19 +1,18 @@
 n = 4
 
-def  toi1(n)
-  if n%2== 1
-    k = 2*n + (n-3)/2
-  else
-    k = 2*n + (n-4)/2
-  end
-  
-  for x in 1..k
-    y = x + x**2
-
-    if  y%10 == 2
-    puts "#{x} + #{x**2} = #{y}"
-    end
-  end
+def toi1(n)
+  k =
+   if n % 2 == 1
+     2 * n + (n - 3) / 2
+   else
+     2 * n + (n - 4) / 2
+   end
+   for x in 1..k
+     y = x + x**2
+      if y % 10 == 2
+        puts "#{x} + #{x**2} = #{y}"
+      end
+   end
 end
 
 toi1(n)
